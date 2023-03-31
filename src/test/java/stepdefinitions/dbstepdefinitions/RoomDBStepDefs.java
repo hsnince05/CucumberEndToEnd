429d07623155c1757b652fdd0dd749f670751e34
Hasan INCE <hsnc0505@gmail.com>
2023-03-31 23:24:44 +0300

    CucumberEndToEnd

---
+++ src/test/java/stepdefinitions/dbstepdefinitions/RoomDBStepDefs.java
@@ -0,0 +1,38 @@
+package stepdefinitions.dbstepdefinitions;
+
+import io.cucumber.java.en.Given;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+import static stepdefinitions.MedunnaRoomStepDefs.roomNumber;
+
+public class RoomDBStepDefs {
+
+    @Given("Olusan odayi room_number ile dogrula")
+    public void olusan_odayi_room_number_ile_dogrula() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://medunna.com:5432/medunna_db_v2", "select_user", "Medunna_pass_@6");
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM room ORDER BY created_date DESC");
+
+//        resultSet.next();
+//        int x = resultSet.getInt(2);
+//        System.out.println("x = " + x);
+//        System.out.println(roomNumber);
+
+        List<Integer> roomNumberList = new ArrayList<>();
+
+        while (resultSet.next()){
+            roomNumberList.add(resultSet.getInt(2));
+        }
+
+        System.out.println("roomNumberList = " + roomNumberList);
+
+        assertTrue(roomNumberList.contains(roomNumber));
+        System.out.println("roomNumber = " + roomNumber);
+
+    }
+}
